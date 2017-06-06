5ed159e30652d1a37d06b8f66d1eb1c72a561c08
Alexey Terekhov <alexey.terekhovv@yandex.ru>
2017-06-06 23:24:27 +0300

    Added missing constants

--- pt/src/utils/consts.rs
+++ pt/src/utils/consts.rs
@@ -4,4 +4,6 @@ pub const UP_VEC: [Real; 3] = [0.0, 1.0, 0.0];
 pub const FORWARD_VEC: [Real; 3] = [0.0, 0.0, -1.0];
 pub const RIGHT_VEC: [Real; 3] = [1.0, 0.0, 0.0];
 
-pub const POSITION_EPSILON: Real = 0.1e-7;
+pub const REAL_EPSILON: Real = ::std::f32::EPSILON as Real;
+pub const POSITION_EPSILON: Real = ::std::f32::EPSILON as Real;
+pub const TEXTURE_INTEGRAL_STEP: Real = 0.1e-2;
